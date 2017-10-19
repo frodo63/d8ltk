462a70d82d9a2e6766990e5cafa0d8c118e91eb0
frodo63 <frodo63@mail.ru>
2017-10-19 11:26:14 +0400

    ready?

---
+++ web/themes/custom/ltk/custom-javascript/headhide.js
@@ -0,0 +1,30 @@
+Drupal.behaviors.basic = {
+    attach: function (context, settings) {
+        (function ($) {
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
+
+                if (scroll > 0) {
+                    $("#navbar").addClass('minimized');
+                    $('#block-sitename div.field--name-body').html('<p>ЛУБРИТЭК</p>');
+                } else {
+                    $("#navbar").removeClass("minimized");
+                    $('#block-sitename div.field--name-body').html('<p>ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ</br>"ЛУБРИТЭК"</p>');
+                }
+            });
+            /*$('#navbar').on( "mouseover", function () {
+                if ($('#navbar').hasClass("minimized")){
+                    $('#navbar').removeClass("minimized");
+                  $('#block-sitename div').html('<p>ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ</br>"ЛУБРИТЭК"</p>');
+                }
+            } );
+          $('#navbar').on( "mouseout", function () {
+            if ($(window).scrollTop() > 0){
+              $('#navbar').addClass("minimized");
+              $('#block-sitename div').html('<p>ЛУБРИТЭК</p>');
+            }
+          } );*/
+
+        })(jQuery);
+    }
+};
