ca68181c959bbb926f5fb05a76a0fb1584364b06
frodo63 <frodo63@mail.ru>
2018-10-30 17:39:46 +0400

    working ? idk

--- public_html/modules/custom/custhtml/src/Controller/custhtmlController.php
+++ public_html/modules/custom/custhtml/src/Controller/custhtmlController.php
@@ -10,44 +10,35 @@ use Drupal\Core\Controller\ControllerBase;
 
 
 class custhtmlController extends ControllerBase {
+
   public function givefrontcontent() {
     return [
       '#theme' => 'custom_front',
-      '#test_var' => 'THIS IS MY CUSTOM VARIABLE!!!',
     ];
   }
+
   public function givecontacts() {
     return [
       '#theme' => 'custom_contacts',
     ];
   }
+
   public function giverequisites() {
     return [
       '#theme' => 'custom_requisites',
     ];
   }
-  public function givepartners() {
-    return [
-      '#theme' => 'custom_partners',
-    ];
-  }
-  public function givelogistics() {
-    return [
-      '#theme' => 'custom_front',
-    ];
-  }
-  public function givenotallowed() {
+
+  public function givefourothree() {
       return [
-          '#theme' => 'custom_403',
-      ];
-  }
-  public function givenotfound() {
-      return [
-          '#theme' => 'custom_404',
+          '#theme' => 'custom_fourothree',
       ];
   }
 
-
-
+  public function givefourofour() {
+      return [
+          '#theme' => 'custom_fourofour',
+      ];
+  }
 
 }
