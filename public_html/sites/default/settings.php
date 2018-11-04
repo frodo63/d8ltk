05c3b27e6bee5431f677e05005863ac79f4ebb1d
frodo63 <frodo63@mail.ru>
2018-11-05 00:43:19 +0400

    Working local development environment

--- public_html/sites/default/settings.php
+++ public_html/sites/default/settings.php
@@ -773,9 +773,9 @@ $settings['entity_update_batch_size'] = 50;
 #
 
 $config_directories['sync'] = '../config/sync';$databases['default']['default'] = array (
-  'database' => 'lubritek_d8ltk',
-  'username' => 'lubritek_d8ltk',
-  'password' => 'Pr5VYT&t',
+  'database' => 'd8ltk-test1',
+  'username' => 'root',
+  'password' => 'tr776bBe',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
